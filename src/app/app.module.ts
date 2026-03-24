ef48593e21108615ec49ef7ba38bebc1cf155266
besha-lgtm <besha@vddigitalalliance.com>
2026-03-24 13:57:29 +0530

    challenges pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { RewardsComponent } from './rewards/rewards.component';
+import { SubmissionComponent } from './submission/submission.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    RewardsComponent,
+    SubmissionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/submission/submission.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-submission',
+  standalone: false,
+  templateUrl: './submission.component.html',
+  styleUrl: './submission.component.css'
+})
+export class SubmissionComponent {
+
+}
