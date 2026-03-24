17bd8bab26f6c6e84bf95e6fa90e82e6acc98885
Rajana Rishi koushik <rishikoushikrajana15@gmail.com>
2026-03-24 15:48:54 +0530

    rewards component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SubmissionComponent } from './submission/submission.component';
+import { RewardsComponent } from './rewards/rewards.component';
 const routes: Routes = [
   {path:'', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'submission', component: SubmissionComponent},
+  {path: 'rewards', component: RewardsComponent}
 ];
 
 @NgModule({
