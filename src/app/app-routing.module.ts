9151242776a17aba9214b138e3fa390c72c1f592
RAJU B SURYANARAYAN <113962337+RAJUBSURYANARAYAN@users.noreply.github.com>
2026-03-25 17:19:13 +0530

    detail sidebar and dashboard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,19 @@ import { LoginComponent } from './login/login.component';
 import { SubmissionComponent } from './submission/submission.component';
 import { RewardsComponent } from './rewards/rewards.component';
 import { RegisterComponent } from './register/register.component';
+import { DetailComponent } from './detail/detail.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 const routes: Routes = [
   {path:'', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'submission', component: SubmissionComponent},
   {path: 'rewards', component: RewardsComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'detail', component: DetailComponent},
+  {path: 'sidebar', component: SidebarComponent},
+  {path: 'dashboard', component: DashboardComponent},
+
 ];
 
 @NgModule({
