1ac213ef09fe675f7ede80fe5560b8e69ad2af81
rohanvarma <ch.rohanvarma4444@gmail.com>
2026-04-10 16:34:06 +0530

    consolidate code changes for improved clarity and maintainability

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { DetailComponent } from '../detail/detail.component';
 
@@ -11,7 +12,23 @@ import { DetailComponent } from '../detail/detail.component';
 export class HeaderComponent {
   menuOpen = false;
 
+  constructor(private router: Router) {}
+
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+    // Prevent body scroll when overlay is open
+    if (this.menuOpen) {
+      document.body.classList.add('overlay-active');
+    } else {
+      document.body.classList.remove('overlay-active');
+    }
+  }
+
+  goToHome() {
+    this.router.navigate(['/home']);
+    // Close menu if open
+    if (this.menuOpen) {
+      this.toggleMenu();
+    }
   }
 }
